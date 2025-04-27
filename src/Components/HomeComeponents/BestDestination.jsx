e6281c386d5f6c8af2a11cebbc6469a2af7038d0
unknown <shaimazerin46@gmail.com>
2025-04-27 13:59:18 +0600

    best destination section done

---
+++ src/Components/HomeComeponents/BestDestination.jsx
@@ -0,0 +1,68 @@
+import { CiTimer } from 'react-icons/ci';
+import nepal from '../../assets/destination/nepal.jpg'
+import taj from '../../assets/destination/tajmahal.jpg'
+import dome from '../../assets/destination/dome.jpg'
+
+const BestDestination = () => {
+    return (
+        <div className="py-20 max-w-7xl mx-auto">
+            <div className="text-center w-1/2 mx-auto mb-20">
+                <h3 className="font-bold text-3xl mb-3"><span className="text-yellow-400">Best </span>Destination</h3>
+                <p>Find your perfect escape among the world’s most stunning destinations. Adventure, relaxation, and unforgettable memories await you</p>
+            </div>
+
+            <div className="grid md:grid-cols-3 grid-cols-1">
+
+                <div className="card bg-base-100 w-96 shadow-sm space-y-3">
+                    <figure>
+                        <img
+                            src={taj}
+                            alt="Tajmahal" className='h-[250px] object-cover w-full'/>
+                    </figure>
+                    <div className="flex justify-between px-3 text-sm">
+                        <p>The Taz Mahal</p>
+                        <p>Agra, Uttar Pradesh, India</p>
+                    </div>
+                    <div className='flex px-3 gap-1 items-center text-sm pb-3'>
+                    <CiTimer />
+                    <p>2 days ago</p>
+                    </div>
+                </div>
+
+                <div className="card bg-base-100 w-96 shadow-sm space-y-3">
+                    <figure>
+                        <img
+                            src={nepal}
+                            alt="Hills" className='h-[250px] object-cover w-full'/>
+                    </figure>
+                    <div className="flex justify-between px-3 text-sm">
+                        <p>Chandragiri Hill</p>
+                        <p>Kathmundu Valley, Nepal</p>
+                    </div>
+                    <div className='flex px-3 gap-1 items-center text-sm pb-3'>
+                    <CiTimer />
+                    <p>2 days ago</p>
+                    </div>
+                </div>
+
+                <div className="card bg-base-100 w-96 shadow-sm space-y-3">
+                    <figure>
+                        <img
+                            src={dome}
+                            alt="Masjid" className='h-[250px] object-cover w-full'/>
+                    </figure>
+                    <div className="flex justify-between px-3 text-sm">
+                        <p>The Dome of the Rock</p>
+                        <p>Temple Mount, Jerusalem</p>
+                    </div>
+                    <div className='flex px-3 gap-1 items-center text-sm pb-3'>
+                    <CiTimer />
+                    <p>3 days ago</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BestDestination;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Components/HomeComeponents/Banner";
+import BestDestination from "../Components/HomeComeponents/BestDestination";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <BestDestination></BestDestination>
         </div>
     );
 };
