ec5b4fd14eabe51f5c7476f88d08034115115fe8
unknown <shaimazerin46@gmail.com>
2025-04-27 20:00:14 +0600

    image section done

---
+++ src/Components/HomeComeponents/ImageSection.jsx
@@ -0,0 +1,20 @@
+import img1 from '../../assets/image-section/ice.jpg'
+
+const ImageSection = () => {
+    return (
+        <div className="py-5" style={{backgroundImage: `url(${img1})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+            <div className='max-w-7xl mx-auto space-y-3'>
+            <p className='text-[100px] font-bold text-yellow-400'>السفر</p>
+            <p className='font-bold text-5xl'>Super Deluxe</p>
+            <p className='ml-5'>Days and nights tour</p>
+
+            <div className='w-[600px] mx-auto bg-white p-1 flex justify-between rounded-3xl shadow-xl'>
+                <input placeholder='search here' type="search" className='outline-0 w-full px-5 focus:ouline-0'/>
+                <button className='py-2 px-4 rounded-3xl text-white bg-yellow-400'>Search</button>
+            </div>
+            </div>
+      </div>
+    );
+};
+
+export default ImageSection;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../Components/HomeComeponents/Banner";
 import BestDestination from "../Components/HomeComeponents/BestDestination";
 import CustomerSays from "../Components/HomeComeponents/CustomerSays";
+import ImageSection from "../Components/HomeComeponents/ImageSection";
 import TopPlaces from "../Components/HomeComeponents/TopPlaces";
 import TripSteps from "../Components/HomeComeponents/TripSteps";
 import WhySafara from "../Components/HomeComeponents/WhySafara";
@@ -15,6 +16,7 @@ const Home = () => {
             <TopPlaces></TopPlaces>
             <WhySafara></WhySafara>
             <CustomerSays></CustomerSays>
+            <ImageSection></ImageSection>
         </div>
     );
 };
